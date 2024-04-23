445837644d0414d1d3e704de7958cf00ee4307db
priyanka <pandharbalepr1997@gmail.com>
2024-04-23 12:15:02 +0530

    completed password generator project

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,35 +1,104 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
-
-import { useState ,useCallback} from 'react'
-import './App.css'
+import { useState, useCallback, useEffect ,useRef} from "react";
 
 function App() {
-  
   const [length, setlength] = useState(8);
-  const [numberAllowed,setNumberAllowed] = useState(false);
-  const [characterAllowed ,setCharAllowed] = useState(false);
-  const [password,setPassword] = useState("");
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef Hook
+  const passwordRef= useRef(null)
 
   // eslint-disable-next-line no-undef
-  const passwordGenerator=useCallback(
-    () =>{
-      let pass=""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy"
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
 
-    if (numberAllowed) str += "0123456789"
-    if (numberAllowed) str += "!@#$%^&*()+={}~`"
-      
-    },[length,numberAllowed,characterAllowed,setPassword]
-    
-  )
-  
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*()+={}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,101);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
 
   return (
     <>
-      <h1 className=" text-4xl text-center text-white">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-600">
+        <h1 className=" text-center text-white my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly={true}
+            ref={passwordRef}
+          />
+
+          <button onClick={copyPasswordToClipboard}  className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex item-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setlength(e.target.value);
+              }}
+            />
+            <label className="text-white">Length:{length}</label>
+          </div>
+
+          <div className="flex item-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+              className="cursor-pointer"
+            />
+            <label className="text-white">Numbers</label>
+          </div>
+
+          <div className="flex item-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={characterAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+              className="cursor-pointer"
+            />
+            <label className="text-white">Characters</label>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
